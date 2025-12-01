f0def5f16a71f6150ba1b81283f59e3ba9effa5c
Gabriel Moreira <gabriel.moreira@escolar.ifrn.edu.br>
2025-12-01 14:58:06 +0000

    01/12

--- biblioteca/routes/index.js
+++ biblioteca/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../utils/db');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -21,5 +22,41 @@ router.get('/agora', function(req,res){
 
 
 })
+
+router.get('/saudar/:nome', function(req, res){
+
+  var msg = 'Olá ' + req.params.nome + '! Seja bem-vindo(a)!<br><br><a href="http://localhost:3000/"><button>Página Inicial</button></a>';
+  res.send(msg)
+})
+
+router.get('/calcular-imc', function(req, res) {
+
+var peso = req.query.peso;
+var estatura = req.query.estatura;
+
+var imc = peso / Math.pow(estatura, 2);
+var msg = '<h3>Seu IMC é ' + imc.toFixed(2) + '</h3>';
+res.send(msg);
+// localhost:3000/calcular-imc/?peso=87&estatura=1.82
+});
+
+router.get('/livros/listar', function(req, res) {
+
+  var cmd = `
+  SELECT id_livro, titulo, ano, nome
+	  FROM tb_livro INNER JOIN tb_editora
+	    ON tb_editora.id_editora = tb_livro.id_editora
+  ORDER BY titulo;`
+
+  db.query(cmd, [], function(erro, listagem){
+    if (erro){
+      res.send(erro);
+    }
+    res.send(listagem);
+  });
+
+});
+
+
 
 module.exports = router;
